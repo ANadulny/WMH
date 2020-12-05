af5d73c9f51795a9cfb59caeb4462c8073019ed2
ANadulny <adrian7991@wp.pl>
2020-12-05 09:33:32 +0100

    move sudoku data into java module and prepare test class

---
+++ SudokuSolver/src/test/java/SudokuReaderTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class SudokuReaderTest {
+
+    @Test
+    public void testSudokuEasyLevelReader() {
+        fail("Sudoku incorrect. Numbers in all rows and columns must be different.");
+    }
+
+    @Test
+    public void testSudokuMiddleLevelReader() {
+        fail("Sudoku incorrect. Numbers in all rows and columns must be different.");
+    }
+
+    @Test
+    public void testSudokuHardLevelReader() {
+        fail("Sudoku incorrect. Numbers in all rows and columns must be different.");
+    }
+}
