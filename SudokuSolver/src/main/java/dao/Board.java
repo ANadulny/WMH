35edb3e93ddc4a03a402e028e2e283451ccd9421
Tomasz Sałacki <salackitomasz@gmail.com>
2020-12-19 10:35:11 +0100

    Move from int to cells

--- SudokuSolver/src/main/java/dao/Board.java
+++ SudokuSolver/src/main/java/dao/Board.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Board {
-    private int [][] board;
+    private Cell [][] board;
 
-    public Board(int [][] initialBoard) {
+    public Board(Cell [][] initialBoard) {
         board = initialBoard;
     }
 
@@ -16,24 +16,24 @@ public class Board {
         return board.length;
     }
 
-    public int [][] getBoard() {
+    public Cell [][] getBoard() {
         return board;
     }
 
     public void insert(Position position, int value) {
-        board[position.x][position.y] = value;
+        board[position.x][position.y].set(value);
     }
 
     public void swapNumbersByPosition(Position p1, Position p2) {
-        int tmp = board[p1.x][p1.y];
-        board[p1.x][p1.y] = board[p2.x][p2.y];
-        board[p2.x][p2.y] = tmp;
+        int tmp = board[p1.x][p1.y].get();
+        board[p1.x][p1.y].set(board[p2.x][p2.y].get());
+        board[p2.x][p2.y].set(tmp);
     }
 
     public BoarderCellsList getRow(int number) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            numbers.add(board[number][i]);
+            numbers.add(board[number][i].get());
         }
         return new BoarderCellsList(numbers);
     }
@@ -41,7 +41,7 @@ public class Board {
     public BoarderCellsList getColumn(int number) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            numbers.add(board[i][number]);
+            numbers.add(board[i][number].get());
         }
         return new BoarderCellsList(numbers);
     }
--- SudokuSolver/src/main/java/dao/Cell.java
+++ SudokuSolver/src/main/java/dao/Cell.java
@@ -3,32 +3,43 @@ package dao;
 // TODO use it in project ?
 public class Cell {
     private int value;
-    private boolean filled;
+    private boolean isOriginal;
 
     public Cell(){
         this(0);
     }
 
     public Cell(int value){
-        filled = false;
         this.value = value;
     }
 
-    public boolean isFilled() {
-        return filled;
+    public Cell(int value, boolean isOriginal){
+        this.value = value;
+        this.isOriginal = isOriginal;
     }
+
 
     public int get() {
         return value;
     }
 
     public void set(final int number){
-        filled = true;
         value = number;
+    }
+
+    public boolean isOriginal(){
+        return this.isOriginal;
     }
 
     public void clear() {
         value = 0;
-        filled = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "value=" + value +
+                ", isOriginal=" + isOriginal +
+                '}';
     }
 }
