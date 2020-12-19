db0e21a39112a3be85156e163b34d85fc37f97d8
Tomasz Sałacki <salackitomasz@gmail.com>
2020-12-19 11:15:29 +0100

    some refactoring and added function to fill board with proper numbers for each subgrid

--- SudokuSolver/src/main/java/dao/Board.java
+++ SudokuSolver/src/main/java/dao/Board.java
@@ -8,8 +8,8 @@ import java.util.stream.Collectors;
 public class Board {
     private Cell [][] board;
 
-    public Board(Cell [][] initialBoard) {
-        board = initialBoard;
+    public Board(Cell [][] board) {
+        this.board = board;
     }
 
     public int size() {
@@ -21,19 +21,19 @@ public class Board {
     }
 
     public void insert(Position position, int value) {
-        board[position.x][position.y].set(value);
+        board[position.x][position.y].setValue(value);
     }
 
     public void swapNumbersByPosition(Position p1, Position p2) {
-        int tmp = board[p1.x][p1.y].get();
-        board[p1.x][p1.y].set(board[p2.x][p2.y].get());
-        board[p2.x][p2.y].set(tmp);
+        int tmp = board[p1.x][p1.y].getValue();
+        board[p1.x][p1.y].setValue(board[p2.x][p2.y].getValue());
+        board[p2.x][p2.y].setValue(tmp);
     }
 
     public BoarderCellsList getRow(int number) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            numbers.add(board[number][i].get());
+            numbers.add(board[number][i].getValue());
         }
         return new BoarderCellsList(numbers);
     }
@@ -41,9 +41,42 @@ public class Board {
     public BoarderCellsList getColumn(int number) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            numbers.add(board[i][number].get());
+            numbers.add(board[i][number].getValue());
         }
         return new BoarderCellsList(numbers);
+    }
+
+    public void fillZeroes(){
+        //x - 0-2,3-5,6-8       0-0, 1-3, 2-6
+        //y - 0-2,3-5,6-8
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                List<Integer> valuesForSubgrid = this.getValuesForSubgrid(i,j);
+                for(int x = i*3; x < (i+1)*3; x++){
+                    for(int y = j*3; y < (j+1)*3; y++){
+                        if(this.board[x][y].getValue() == 0){
+                            this.board[x][y].setValue(valuesForSubgrid.get(0));
+                            valuesForSubgrid.remove(valuesForSubgrid.get(0));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Integer> getValuesForSubgrid(int i, int j){
+        List<Integer> valuesForSubgrid = new ArrayList<>();
+        for(int k = 1; k < 10; k++){
+            valuesForSubgrid.add(k);
+        }
+        for(int x = i*3; x < (i+1)*3; x++){
+            for(int y = j*3; y < (j+1)*3; y++){
+                if(this.board[x][y].getValue() != 0)
+                    valuesForSubgrid.remove((Object)this.board[x][y].getValue());
+            }
+        }
+        return valuesForSubgrid;
     }
 
     @Override
