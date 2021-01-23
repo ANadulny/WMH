144f849ca199fde784843f99f449b11987a8d0f8
ANadulny <adrian7991@wp.pl>
2021-01-23 20:29:56 +0100

    fix returning best state during solviong sudoku

--- SudokuSolver/src/main/java/sudoku/run/SudokuSolverTesting.java
+++ SudokuSolver/src/main/java/sudoku/run/SudokuSolverTesting.java
@@ -1,7 +1,10 @@
 package sudoku.run;
 
+import dao.Board;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sudoku.reader.SudokuReader;
+import sudoku.solver.SudokuSolver;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,5 +20,16 @@ public class SudokuSolverTesting {
             SudokuSolverThread T1 = new SudokuSolverThread(size);
             T1.start();
         }
+
+//        singleSudokuSolverTest("easy","1.txt", 900, 200, 200, 5, 2, false);
+    }
+
+    public static void singleSudokuSolverTest(String level, String fileName, int maxIterations, int tabuSize, int memSize, int maxBoardFrequency, int aspirationCriterionNum, boolean fillBoardWithRandomNumbers) {
+        SudokuReader sr = new SudokuReader();
+        Board testBoard = sr.readSudokuFromFile(level, "tests", fileName);
+        SudokuSolver solver = new SudokuSolver(testBoard, maxIterations, tabuSize, memSize, maxBoardFrequency, aspirationCriterionNum);
+        solver.solveSudoku(fillBoardWithRandomNumbers);
+        logger.info("Last iteration " + solver.getLastIteration());
+        logger.info(solver.getBestResult().toString());
     }
 }
--- SudokuSolver/src/main/java/sudoku/solver/SudokuSolver.java
+++ SudokuSolver/src/main/java/sudoku/solver/SudokuSolver.java
@@ -14,32 +14,28 @@ public class SudokuSolver {
     private final Logger logger = LoggerFactory.getLogger(SudokuSolver.class);
     private final TabuList tabuList;
     private final MemoryList memoryList;
-    private final int aspirationCriterioNum;
+    private final int aspirationCriterionNum;
 
     private Board board;
     private Board bestResult;
     private int lastIteration = 0;
 
-    public SudokuSolver(Board board, int maxIterations, int tabuSize, int memSize, int maxBoardFrequency, int aspirationCriterioNum){
+    public SudokuSolver(Board board, int maxIterations, int tabuSize, int memSize, int maxBoardFrequency, int aspirationCriterionNum){
         this.board = board;
         this.maxIterations = maxIterations;
         this.tabuList = new TabuList(tabuSize);
         this.memoryList = new MemoryList(memSize, maxBoardFrequency);
-        this.aspirationCriterioNum = aspirationCriterioNum;
+        this.aspirationCriterionNum = aspirationCriterionNum;
     }
 
     public Board solveSudoku(boolean fillBoardWithRandomNumbers){
         this.board.fillZeroesWithNumbers(fillBoardWithRandomNumbers);
         int iterator = 0;
+        this.bestResult = board;
         while (iterator < maxIterations && this.board.getConflictedPositions() != 0) {
-//            System.out.println(iterator);
-//            logger.info("Iteration [" + (iterator + 1) + "]");
-//            logger.info("Generating movements:");
             List<Movement> movementList = this.board.generateAllMovements();
-//            logger.info(movementList.toString());
             List<Board> neighbours = new ArrayList<>();
-//            logger.info("Searching neighbours:");
-            for(Movement movement : movementList){
+            for (Movement movement : movementList) {
                 Cell[][] cells = Arrays.stream(this.board.getBoard()).map(Cell[]::clone).toArray(Cell[][]::new);
                 Board board = new Board(cells).executeMovement(movement);
                 board.calculateNumberOfConflictedPosition();
@@ -49,41 +45,49 @@ public class SudokuSolver {
             neighbours.sort(Comparator.comparingInt(Board::getConflictedPositions));
             if (neighbours.size() <= 0) {
                 logger.error("Neighbour list size is <= 0");
-                return this.board;
+                return this.bestResult;
             }
 
+            Board bestIterationState = null;
+            if (neighbours.get(0).getConflictedPositions() == 0) {
+                bestIterationState = neighbours.get(0);
+            }
             int i = 0;
-            boolean isFoundFollowingBoard = false;
             Board bestTabuState = null;
-            while (i < neighbours.size() && !isFoundFollowingBoard) {
-//                logger.info("Neighbour on position: [" + i + "]");
-//                logger.info(neighbours.get(i).toString());
+            while (i < neighbours.size() && bestIterationState == null) {
                 if (!memoryList.isBoardFrequenciesOk(neighbours.get(i))) {
 //                    logger.info("Memory list frequency is not ok");
                 } else if (tabuList.hasBoardState(neighbours.get(i))) {
                     if (bestTabuState == null) {
                         bestTabuState = neighbours.get(i);
-//                        logger.info("Setting best tabu neighbour state  for: " + bestTabuState);
                     }
                 } else if (bestTabuState != null && isAspirationCriterionFulfilled(bestTabuState.getConflictedPositions(), neighbours.get(i).getConflictedPositions())) {
-//                    logger.info("Tabu conflict positions = " + bestTabuState.getConflictedPositions() + "; Non tabu conflict positions = " + neighbours.get(i).getConflictedPositions());
-                    this.board = bestTabuState;
                     tabuList.updatePositionInTabu(bestTabuState);
-                    isFoundFollowingBoard = true;
+                    bestIterationState = bestTabuState;
                 } else {
-//                    logger.info("Non on tabu state conflict positions = " + neighbours.get(i).getConflictedPositions());
-                    this.board = neighbours.get(i);
                     tabuList.addElement(neighbours.get(i));
-                    isFoundFollowingBoard = true;
+                    bestIterationState = neighbours.get(i);
                 }
                 i++;
             } // end while loop
-//            logger.info("Chosen board: " + this.board);
+
+            if (iterator == 829) {
+                System.out.println("TEST");
+            }
+
+            if (bestIterationState == null) {
+                logger.error("bestIterationState can not be null!");
+                return this.bestResult;
+            }
+
+            if (bestIterationState.getConflictedPositions() < this.bestResult.getConflictedPositions()) {
+                this.bestResult = bestIterationState;
+            }
+            this.board = bestIterationState;
             memoryList.addBoard(this.board);
             iterator++;
         }  // end while loop
         this.lastIteration = iterator;
-        this.bestResult = this.board;
 //        logger.info("Ending iteration: " + iterator);
         return this.bestResult;
     }
@@ -93,7 +97,7 @@ public class SudokuSolver {
     }
 
     private boolean isAspirationCriterionFulfilled(int tabuConflictedPositions, int neighbourConflictedPositions) {
-        return tabuConflictedPositions <= neighbourConflictedPositions - aspirationCriterioNum;
+        return tabuConflictedPositions <= neighbourConflictedPositions - aspirationCriterionNum;
     }
 
     public int getLastIteration() {
